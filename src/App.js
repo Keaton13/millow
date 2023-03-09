8789da8a1415e1e0bc60474c87a19e4f80524e5c
Keaton Krieger <keatonkrieger@Keatons-MacBook-Pro.local>
2023-03-08 17:42:43 -0800

    setup app.js and navigation.js. Also loaded in blockchain data

--- src/App.js
+++ src/App.js
@@ -1,29 +1,42 @@
-import { useEffect, useState } from 'react';
-import { ethers } from 'ethers';
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
 
 // Components
-import Navigation from './components/Navigation';
-import Search from './components/Search';
-import Home from './components/Home';
+import Navigation from "./components/Navigation";
+import Search from "./components/Search";
+import Home from "./components/Home";
 
 // ABIs
-import RealEstate from './abis/RealEstate.json'
-import Escrow from './abis/Escrow.json'
+import RealEstate from "./abis/RealEstate.json";
+import Escrow from "./abis/Escrow.json";
 
 // Config
-import config from './config.json';
+import config from "./config.json";
 
 function App() {
+  const [account, setAccount] = useState(null);
+
+  useEffect(() => {
+    loadBlockChainData();
+  }, []);
+
+  const loadBlockChainData = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+    window.ethereum.on('accountsChanged', async () => {
+      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+      const account = ethers.utils.getAddress(accounts[0])
+      setAccount(account);
+    })
+  };
 
   return (
     <div>
-
-      <div className='cards__section'>
+      <div className="cards__section">
+        <Navigation account={account} setAccount={setAccount}/>
 
         <h3>Welcome to Millow</h3>
-
       </div>
-
     </div>
   );
 }
